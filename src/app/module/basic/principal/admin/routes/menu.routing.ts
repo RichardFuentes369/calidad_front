dab9e213f5e53048b963e3b7be62855a92271e44
RichardFuentes369 <javierbaron6@gmail.com>
2025-03-25 22:13:40 -0500

    ajustes

--- src/app/module/basic/principal/admin/routes/menu.routing.ts
+++ src/app/module/basic/principal/admin/routes/menu.routing.ts
@@ -1,10 +1,5 @@
 import { Routes } from '@angular/router';
 
-import { adminGuard } from '@guard/roles/admin/admin.guard';
-
-// componentes
-// import { IndexComponent as PublicacionesIndex } from '@module/posts/index/index.component';
-
 export const MenuRoutes: Routes = [
   // Modulo Usuarios
   {
@@ -27,21 +22,20 @@ export const MenuRoutes: Routes = [
     path: 'index-mantenimiento',
     title: 'Mantenimiento',
     data: { breadcrumb: 'Mantenimiento' },
-    loadChildren: () => import('@module/mantenimiento/routes/administrador.routing').then(x=>x.MantenimientoRouter)
+    loadChildren: () => import('@module/mantenimiento/routes/index.routing').then(x=>x.MantenimientoRouter)
   },
   // Modulo Zona Comun
   {
     path: 'index-zona-comun',
     title: 'Zona Comun',
     data: { breadcrumb: 'Zona Comun' },
-    loadChildren: () => import('@module/zonaComun/routes/administrador.routing').then(x=>x.ZonaComunRouter)
+    loadChildren: () => import('@module/zonaComun/routes/intex.routing').then(x=>x.ZonaComunRouter)
   },
   // Modulo Proveedor
   {
     path: 'index-proveedor',
     title: 'Proveedor',
     data: { breadcrumb: 'Proveedor' },
-    loadChildren: () => import('@module/proveedor/routes/administrador.routing').then(x=>x.ProveedorRouter)
+    loadChildren: () => import('@module/proveedor/routes/index.routing').then(x=>x.ProveedorRouter)
   },
-
 ];
